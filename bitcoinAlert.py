82bbe4aac93d53783dd51c97ae022d88bdeb4e0e
Mz <rebel.king700@gmail.com>
2020-04-24 22:21:00 +0430

    add sending email with delay

--- bitcoinAlert.py
+++ bitcoinAlert.py
@@ -23,3 +23,34 @@ def send_email():
   server.quit()
   print("successfully sent email to %s:" % (msg['To']))
   print("Price of bitcoin was at " + str(bitcoin_rate))
+your_name = input('Enter your name: ')
+your_email = input('Enter your email address (gmail only): ')
+your_password = getpass.getpass()
+send_email_to = input('Enter email address to send to: ')
+alert_amount = input('Alert if Bitcoin drops below: ')
+alert_amounthigher = input('Alert if Bitcoin increase more than: ')
+
+while True:
+  url = "https://api.coindesk.com/v1/bpi/currentprice.json"
+  response = requests.get(
+    url, 
+    headers={"Accept": "application/json"},
+  )
+  data = response.json()
+  bpi = data['bpi']
+  USD = bpi['USD']
+  bitcoin_rate = int(USD['rate_float'])
+  if bitcoin_rate > int(alert_amounthigher):
+      send_email()
+      print('will check in 3 minutes. Ctr + C to quit.')
+      time.sleep(180)
+    else:
+        time.sleep(300)
+        print('Price is ' + str(bitcoin_rate) + '. Will check again in 5 minutes. Ctrl + C to quit.')
+  if bitcoin_rate < int(alert_amount):
+    send_email()
+    print('Will check again in 3 minutes. Ctrl + C to quit.')
+    time.sleep(180)
+  else:
+    time.sleep(300)
+    print('Price is ' + str(bitcoin_rate) + '. Will check again in 5 minutes. Ctrl + C to quit.')
