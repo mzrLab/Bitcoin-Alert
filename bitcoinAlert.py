5cdd0b5e7cf4d651fc8de1850d3d3eef8850175c
Mz <rebel.king700@gmail.com>
2020-04-25 00:00:41 +0430

    edit customization feature

--- bitcoinAlert.py
+++ bitcoinAlert.py
@@ -7,20 +7,21 @@ import time
 
 response = requests.get('https://api.nomics.com/v1/currencies/ticker?key=bcbd33571b255a696995dad391a822e6&ids=BTC&interval=1d,30d&convert=USD')
 data = response.json()
+bit = data[0]['price']
+ask = input('do you want to customize price? (Y/N)')
+if str(ask) == "Y":
+  alert_amount = input('Alert if Bitcoin drops below: ')
+  alert_amounthigher = input('Alert if Bitcoin increase')
+while True:
+  if alert_amount > bit:
+    print("price is droped below:" + alert_amount + "price is now :"+ bit)
+    time.sleep(180)
+  if alert_amount < bit:
+    print("price is Increased :" + alert_amounthigher + "price is now :"+ bit)
+    time.sleep(180)
 def PrintPrice(Bitcoin):
   return print("Okay - Bitcoin Price is :"+ Bitcoin)
-bit = data[0]['price']
-def Ask():
-  bitcoinPrice = data[0]['price']
-  ask = input('do you want to customize price? (Y/N)')
-  if str(ask) == "Y":
-    alert_amount = input('Alert if Bitcoin drops below: ')
-    alert_amounthigher = input('Alert if Bitcoin increase') 
-
-
-  if str(ask) == "N":
-    while True:
-      PrintPrice(bit)
-      time.sleep(180)
-Ask()
-playsound('alert.mp3')
+if str(ask) == "N":
+  while True:
+    PrintPrice(bit)
+    time.sleep(180)
